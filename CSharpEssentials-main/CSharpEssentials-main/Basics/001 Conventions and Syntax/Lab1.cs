6e185ddd809a08f24f61b428d074b5a02b0ae0c9
yinniethe1st <yinniethe1st@yahoo.com>
2022-05-26 23:38:07 -0600

    practice

---
+++ CSharpEssentials-main/CSharpEssentials-main/Basics/001 Conventions and Syntax/Lab1.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     string name = "John";
+      int myNum = 15;
+      
+      Console.WriteLine(myNum);
+      Console.WriteLine(name);
+
+    int myNum2= 16;
+      myNum2 = 20;
+      Console.WriteLine(myNum2);
+
+
+     int myNum3 = 5;               // integer (whole number)
+      double myDoubleNum3 = 5.99D;  // floating point number
+      char myLetter = 'D';         // character
+      bool myBool = true;          // boolean
+      string myText = "Hello";     // string
+      Console.WriteLine(myNum3);
+      Console.WriteLine(myDoubleNum3);
+      Console.WriteLine(myLetter);
+      Console.WriteLine(myBool);
+      Console.WriteLine(myText);
+
+    int myNum4 = 100000;
+      Console.WriteLine(myNum4);
+
+    long myNum5 = 15000000000L;
+      Console.WriteLine(myNum5);
+
+    float myNum6 = 5.75F;
+      Console.WriteLine(myNum6);
+
+    double myNum7 = 19.99D;
+    Console.WriteLine(myNum7);
+
+
+
+     }
+
+  }
+}
