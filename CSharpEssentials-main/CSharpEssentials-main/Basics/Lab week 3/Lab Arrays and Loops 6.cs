56fd8262007da3af49603b7d1009782fd38e7c16
yinniethe1st <yinniethe1st@yahoo.com>
2022-06-08 21:39:25 -0600

    weapon

--- CSharpEssentials-main/CSharpEssentials-main/Basics/Lab week 3/Lab Arrays and Loops 6.cs
+++ CSharpEssentials-main/CSharpEssentials-main/Basics/Lab week 3/Lab Arrays and Loops 6.cs
@@ -3,9 +3,10 @@ public class Program
 {
     public static void Main() 
     {
-    weapon[] weaponsObjs = {new weapon(), new weapon()};
+    weapon[] weaponsObjs = {new weapon(), new weapon(), new weapon()};
 
     weaponsObjs[1].weaponName = "Sword";
+    weaponsObjs[2].weaponName = "Hammer";
 
     for(var i=0; i<weaponsObjs.Length; i++) {
         weaponsObjs[i].powerLevel = 0;
