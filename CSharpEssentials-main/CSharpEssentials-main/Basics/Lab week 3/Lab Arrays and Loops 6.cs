ccb7b569036f635694d848310037e9f01566e3d8
yinniethe1st <yinniethe1st@yahoo.com>
2022-06-08 21:38:25 -0600

    add weapon lab

---
+++ CSharpEssentials-main/CSharpEssentials-main/Basics/Lab week 3/Lab Arrays and Loops 6.cs
@@ -0,0 +1,25 @@
+using System;
+public class Program
+{
+    public static void Main() 
+    {
+    weapon[] weaponsObjs = {new weapon(), new weapon()};
+
+    weaponsObjs[1].weaponName = "Sword";
+
+    for(var i=0; i<weaponsObjs.Length; i++) {
+        weaponsObjs[i].powerLevel = 0;
+    }
+
+    foreach(var item in weaponsObjs) 
+        {
+        Console.WriteLine(item.weaponName);
+        Console.WriteLine(item.powerLevel);
+        }
+    }
+}
+
+public class weapon {
+    public string weaponName = "Axe";
+    public int powerLevel = 1;
+}
