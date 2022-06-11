80ec444cad19a72b2ae9f4d8519f001174f07dae
yinniethe1st <yinniethe1st@yahoo.com>
2022-06-10 23:45:27 -0600

    message

--- CSharpEssentials-main/CSharpEssentials-main/Basics/Lab week 3/Lab Arrays and Loops 6.cs
+++ CSharpEssentials-main/CSharpEssentials-main/Basics/Lab week 3/Lab Arrays and Loops 6.cs
@@ -1,26 +1,21 @@
 using System;
-public class Program
+
+namespace Loop
 {
-    public static void Main() 
-    {
-    weapon[] weaponsObjs = {new weapon(), new weapon(), new weapon()};
-
-    weaponsObjs[1].weaponName = "Sword";
-    weaponsObjs[2].weaponName = "Hammer";
-
-    for(var i=0; i<weaponsObjs.Length; i++) {
-        weaponsObjs[i].powerLevel = 0;
-    }
-
-    foreach(var item in weaponsObjs) 
-        {
-        Console.WriteLine(item.weaponName);
-        Console.WriteLine(item.powerLevel);
-        }
-    }
-}
-
-public class weapon {
-    public string weaponName = "Axe";
-    public int powerLevel = 1;
+	class NestedForLoop
+	{
+		public static void Main(string[] args)
+		{
+			int a = 0, b = 0;
+			for (int i = 1; i < 10 ;  i++ )
+			{
+				a++;
+				for (int j = 1; j < 5; j++)
+				{
+					b++;
+				}
+			}
+			Console.WriteLine("{0},{1}", a, b);
+		}
+	}
 }
